a34766c09f662ed0f4986ab9054888ff7c43d363
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2015-02-17 17:41:20 -0500

    added a switch statement to count balloons

---
+++ 99RedBalloons/Balloon.swift
@@ -0,0 +1,15 @@
+//
+//  Balloon.swift
+//  99RedBalloons
+//
+//  Created by Daniel Kwiatkowski on 2015-02-17.
+//  Copyright (c) 2015 Daniel Kwiatkowski. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Balloon {
+    var number = 0 
+    var image = UIImage(named: "")
+}
--- 99RedBalloons/ViewController.swift
+++ 99RedBalloons/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var balloonsLabel: UILabel!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    
+    var balloons:[Balloon] = []//array of instances
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nextBalloonBarButtonItemPressed(sender: UIBarButtonItem) {
+    }
 
+    //this function shows a random photo based on the random number showed in the for loop
+    //practice my switch statement
+    func createBalloons () {
+        for var balloonCount = 0; balloonCount <= 99; ++balloonCount {
+            var balloon = Balloon()
+            balloon.number = balloonCount
+            let randomNumber = Int(arc4random_uniform(UInt32(4)))
+            
+            switch randomNumber {
+            case 1 :
+                balloon.image = UIImage(named: "RedBalloon1.jpg")
+            case 2 :
+                balloon.image = UIImage(named: "RedBalloon2.jpg")
+            case 3:
+                balloon.image = UIImage(named: "RedBalloon3.jpg")
+            default:
+                balloon.image = UIImage(named: "RedBalloon4.jpg")
+            }
+            
+            self.balloons.append(balloon)
+        }
+    }
 }
 
